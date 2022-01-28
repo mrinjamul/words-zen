24f28d8f03674b4bc4340dfa22e3f3314889dbbb
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2022-01-28 14:24:35 +0530

    feat: Add views in go binary

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -1,26 +1,38 @@
 package routes
 
 import (
+	"embed"
+	"io/fs"
+	"log"
 	"net/http"
-	"path"
-	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 	"github.com/mrinjamul/words-zen/api/handler"
 )
 
+// ViewsFs for static files
+var ViewsFs embed.FS
+
 func InitRoutes(routes *gin.Engine) {
 	// Serve the frontend
 	// This will ensure that the angular files are served correctly
-	routes.NoRoute(func(ctx *gin.Context) {
-		dir, file := path.Split(ctx.Request.RequestURI)
-		ext := filepath.Ext(file)
-		if file == "" || ext == "" {
-			ctx.File("./views" + "/index.html")
-		} else {
-			ctx.File("./views" + "/" + path.Join(dir, file))
-		}
-	})
+	fsRoot, err := fs.Sub(ViewsFs, "views")
+	if err != nil {
+		log.Println(err)
+	}
+	routes.NoRoute(gin.WrapH(http.FileServer(http.FS(fsRoot))))
+	// routes.StaticFS("/", http.FS(fsRoot))
+
+	// routes.NoRoute(func(ctx *gin.Context) {
+	// 	dir, file := path.Split(ctx.Request.RequestURI)
+	// 	ext := filepath.Ext(file)
+	// 	if file == "" || ext == "" {
+	// 		ctx.File("./views" + "/index.html")
+	// 	} else {
+	// 		ctx.File("./views" + "/" + path.Join(dir, file))
+	// 	}
+	// })
+
 	// health check
 	routes.GET("/api/health", func(c *gin.Context) {
 		c.JSON(
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"embed"
 	"log"
 	"os"
 
@@ -8,6 +9,9 @@ import (
 
 	"github.com/gin-gonic/gin"
 )
+
+//go:embed views/*
+var viewsFs embed.FS
 
 func main() {
 	// Get port from env
@@ -21,6 +25,7 @@ func main() {
 	// Set the router as the default one shipped with Gin
 	server := gin.Default()
 	// Initialize the routes
+	routes.ViewsFs = viewsFs
 	routes.InitRoutes(server)
 	// Start and run the server
 	log.Fatal(server.Run(port))
